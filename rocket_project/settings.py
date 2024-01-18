c245ba102e8248a4efff149d294c4b4c3f3be978
Arii D <108074652+ar11tehau@users.noreply.github.com>
2024-01-18 22:02:11 +0100

    setting edit

--- rocket_project/settings.py
+++ rocket_project/settings.py
@@ -18,12 +18,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'web/static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'web/static'),
+# ]
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
